1fd2639a6eebdf8e5f629be9428bb17a4dcf2d7d
Leonardo Santos <kuhnnhuk@outlook.com>
2023-06-05 06:50:20 -0300

    feat: task 1 done

---
+++ utils.py
@@ -0,0 +1,45 @@
+from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitlesError
+from datetime import datetime
+
+data = {
+    "name": "França",
+    "titles": 4,
+    "top_scorer": "Zidane",
+    "fifa_code": "FRA",
+    "first_cup": "2002-10-18",
+}
+
+
+def data_processing(data: dict):
+    if data["titles"] < 0:
+        raise NegativeTitlesError("titles cannot be negative")
+
+    data_sent = str(data["first_cup"])
+    sliced_data_sent = int(data_sent[0:4])
+
+    year_now = str(datetime.now())
+    sliced_year = int(year_now[0:4])
+
+    cup_years = [1930]
+
+    for n in cup_years:
+        n = n + 4
+        if n < sliced_year:
+            cup_years.append(n)
+
+    if sliced_data_sent in cup_years:
+        total_cups = len(cup_years)
+        index_this_cup = cup_years.index(sliced_data_sent)
+        possible_cups = total_cups - index_this_cup - 1
+        print(possible_cups)
+
+        if data["titles"] > possible_cups:
+            raise ImpossibleTitlesError(
+                "impossible to have more titles than disputed cups"
+            )
+
+    else:
+        raise InvalidYearCupError("there was no world cup this year")
+
+
+data_processing(data)
